8e0a7225c7b1e95338d31e487363f57b995fcc87
Jordan Garcia <arickho@gmail.com>
2025-01-22 20:51:43 -0500

    initial commit

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,26 @@
+import * as testing from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { App } from "supertest/types";
+import { AppModule } from "../src/app/app.module";
+
+describe("AppController (e2e)", () => {
+  let app: INestApplication<App>;
+
+  beforeEach(async () => {
+    const moduleFixture: testing.TestingModule =
+      await testing.Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it("/ (GET)", () => {
+    return request(app.getHttpServer())
+      .get("/")
+      .expect(200)
+      .expect("Hello World!");
+  });
+});
